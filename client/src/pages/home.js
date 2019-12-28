221a9107ebcda94679465952166824e81c56abd8
Mat Hayward <xybolx@Mats-MacBook-Pro.local>
2019-12-27 17:53:01 -0700

    gamepad support in home

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -1,9 +1,10 @@
-import React, { useState } from 'react';
+import React from 'react';
 import useFizzBuzz from '../hooks/useFizzBuzz';
 import Title from '../components/title';
 import NavBtn from '../components/buttons/NavBtn';
 import PageContainer from '../components/pageContainer';
 import CenteredColumn from '../components/centeredColumn';
+import Gamepad from 'react-gamepad';
 
 const Home = () => {
 
@@ -14,9 +15,65 @@ const Home = () => {
     const numbers = useFizzBuzz(200);
 
     console.log(numbers);
+
+    // handle gamepad controls
+    const connectHandler = gamepadIndex => {
+        setGamepadConnected(true);
+        console.log(`Gamepad ${gamepadIndex + 1} connected !`);
+      };
+     
+      const disconnectHandler = gamepadIndex => {
+          setGamepadConnected(false);
+        console.log(`Gamepad ${gamepadIndex + 1} disconnected !`);
+      };
+     
+      const buttonChangeHandler = (buttonName, down) => {
+        console.log(buttonName, down)
+      };
+     
+      const axisChangeHandler = (axisName, value, previousValue) => {
+        console.log(axisName, value)
+      };
+     
+      const buttonDownHandler = buttonName => {
+        console.log(buttonName, 'down')
+      };
+     
+      const buttonUpHandler = buttonName => { 
+        console.log(buttonName, 'up')
+      };
+
+      const startHandler = () => {
+        window.location = "/instructions";
+      };
     
     return (
         <PageContainer className="home-earth">
+            <Gamepad
+                onConnect={connectHandler}
+                onDisconnect={disconnectHandler}
+                onButtonDown={buttonDownHandler}
+                onButtonUp={buttonUpHandler}
+                onButtonChange={buttonChangeHandler}
+                onAxisChange={axisChangeHandler}
+                onA={() => {}}
+                onB={() => {}}
+                onX={() => {}}
+                onY={() => {}}
+                onStart={startHandler}
+                onBack={() => {}}
+                onLT={() => {}}
+                onRT={() => {}}
+                onLB={() => {}}
+                onRB={() => {}}
+                onLS={() => {}}
+                onRS={() => {}}
+                onUp={() => {}}
+                onDown={() => {}}
+                onLeft={() => {}}
+                onRight={() => {}}>
+                <div />
+            </Gamepad>
             <Title>Trump Earth Defense</Title>
             <CenteredColumn className="home-controls">
                 <p>Ready to make Earth great again?</p>
